40a816ac6eafe53a8e44395d3bd02935068ade89
itsMeOnli <aman.mohammed.818@gmail.com>
2026-03-11 15:55:00 +0300

    puzzle 1 solution

--- week1-knowledge/knights/puzzle.py
+++ week1-knowledge/knights/puzzle.py
@@ -12,7 +12,9 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    Implication(And(AKnight,AKnave), AKnight),
+    Implication(Not(And(AKnight, AKnave)), AKnave),
+    Or(AKnave, AKnight)
 )
 
 # Puzzle 1
@@ -43,9 +45,9 @@ def main():
     symbols = [AKnight, AKnave, BKnight, BKnave, CKnight, CKnave]
     puzzles = [
         ("Puzzle 0", knowledge0),
-        ("Puzzle 1", knowledge1),
-        ("Puzzle 2", knowledge2),
-        ("Puzzle 3", knowledge3)
+        # ("Puzzle 1", knowledge1),
+        # ("Puzzle 2", knowledge2),
+        # ("Puzzle 3", knowledge3)
     ]
     for puzzle, knowledge in puzzles:
         print(puzzle)
